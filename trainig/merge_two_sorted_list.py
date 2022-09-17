b3cff573e82b831a76d1923879516ee2d247f005
BuJIKuH <klassniy.jew@gmail.com>
2022-09-17 12:50:55 +0300

    update

--- trainig/merge_two_sorted_list.py
+++ trainig/merge_two_sorted_list.py
@@ -10,13 +10,16 @@ def mergeTwoLists(list1: Optional[list],
     res = []
     for i in list1:
         for j in str(i):
-            res.append(j)
+            res.append(j.strip(' ,{}'))
     for i in list2:
         for j in str(i):
-            res.append(j)
+            res.append(j.strip(' ,{}'))
+
+    # res = sorted(res)
     return res
 
-def find_smallest(res):
+
+def find_smallest(res: Optional[list]) -> int:
     smallest = res[0]
     smallest_index = 0
     for i in range(1, len(res)):
@@ -25,15 +28,14 @@ def find_smallest(res):
             smallest_index = i
     return smallest_index
 
-def selectionSort(arr):
-    newArr = []
+
+def selectionSort(arr: Optional[list]) -> Optional[list]:
+    new_list = []
     for i in range(len(arr)):
         smallest = find_smallest(arr)
-        newArr.append(arr.pop(smallest))
-    return newArr
+        new_list.append(arr.pop(smallest))
+    return new_list
 
 
 print(selectionSort(mergeTwoLists([321], [546])))
-
-
-
+# print(mergeTwoLists([321], [546]))
